b75e554b6269b9ce438a7256f8f2a918c55e7a13
danadajian <danadajian@gmail.com>
2019-03-19 16:47:06 -0500

    setting directory for heroku

---
+++ backend/static/react/precache-manifest.40a1af72901933fcc2eda8ed9bf29d58.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "874dc56519fe19a44b4c",
+    "url": "/static/react/css/main.95d363a7.chunk.css"
+  },
+  {
+    "revision": "874dc56519fe19a44b4c",
+    "url": "/static/react/js/main.1e332c60.chunk.js"
+  },
+  {
+    "revision": "53eac5d40d2daff628d2",
+    "url": "/static/react/js/runtime~main.5d7886d3.js"
+  },
+  {
+    "revision": "c57b85acc1cfa4aa3674",
+    "url": "/static/react/css/2.30dfd29d.chunk.css"
+  },
+  {
+    "revision": "c57b85acc1cfa4aa3674",
+    "url": "/static/react/js/2.6070cecd.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "076dc20edf52be6efbb83c7dd09b84fa",
+    "url": "/static/react/media/map-hue.076dc20e.png"
+  },
+  {
+    "revision": "b01e6120d05abd33918d1dbb78c0660f",
+    "url": "/static/react/media/map-saturation-overlay.b01e6120.png"
+  },
+  {
+    "revision": "c42dcedcff18bf965a5f2d2d0ed220c8",
+    "url": "/static/react/../../templates/index.html"
+  }
+];
