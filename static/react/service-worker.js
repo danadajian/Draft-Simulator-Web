40ec414cd5b7a0f28c432aee22c3f3cffbb33e1a
Adajian <dadajian@stats.com>
2019-03-20 16:54:14 -0500

    getting rid of backend folder

--- static/react/service-worker.js
+++ static/react/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/static/react/precache-manifest.40a1af72901933fcc2eda8ed9bf29d58.js"
+  "/static/react/precache-manifest.1bdbfba39d8aa79820a6fc5625eae3bd.js"
 );
 
 workbox.clientsClaim();
