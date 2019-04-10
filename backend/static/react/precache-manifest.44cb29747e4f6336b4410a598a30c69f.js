3eade7d8e9d49a9b2fa87986de044cc63f0be4bb
unknown <danadajian@gmail.com>
2019-04-10 00:53:09 -0500

    Couldn't get individual info bubbles to work with resizing, so just went with consolidated bubbles

---
+++ backend/static/react/precache-manifest.44cb29747e4f6336b4410a598a30c69f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b01e6120d05abd33918d1dbb78c0660f",
+    "url": "/static/react/media/map-saturation-overlay.b01e6120.png"
+  },
+  {
+    "revision": "076dc20edf52be6efbb83c7dd09b84fa",
+    "url": "/static/react/media/map-hue.076dc20e.png"
+  },
+  {
+    "revision": "c5005feba5d92371ab2c34bb97965345",
+    "url": "/static/react/media/football.c5005feb.ico"
+  },
+  {
+    "revision": "53eac5d40d2daff628d2",
+    "url": "/static/react/js/runtime~main.5d7886d3.js"
+  },
+  {
+    "revision": "d6d165835f8128ad6019",
+    "url": "/static/react/js/main.b598671a.chunk.js"
+  },
+  {
+    "revision": "dd27d559d5f3ab03c517",
+    "url": "/static/react/js/2.e424b71f.chunk.js"
+  },
+  {
+    "revision": "d6d165835f8128ad6019",
+    "url": "/static/react/css/main.96c210b8.chunk.css"
+  },
+  {
+    "revision": "dd27d559d5f3ab03c517",
+    "url": "/static/react/css/2.5009caa8.chunk.css"
+  },
+  {
+    "revision": "2ba41244492040fc9f6cc7ee4b75fd31",
+    "url": "/static/react/../../templates/index.html"
+  }
+];
