2d5224444ab78326e65e50d64c03a5e86470a713
Adajian <dadajian@stats.com>
2019-04-23 17:46:56 -0500

    Updated NBA optimizer to limit to players playing today

--- backend/src/main/NBASalaries.py
+++ backend/src/main/NBASalaries.py
@@ -36,37 +36,38 @@ for site in sites:
 
     # find string indices of players and salaries and put them in lists
     for index in dfsPlayerIndexList:
-        dfsLowerNameIndex = dfsText.find('>', index) + 1
-        dfsUpperNameIndex = dfsText.find('<', dfsLowerNameIndex)
-        dfsLowerSalaryIndex = dfsText.find('</font>', dfsUpperNameIndex) - 7
-        dfsPlayer = dfsText[dfsLowerNameIndex: dfsUpperNameIndex]
-        dfsSalaryText = dfsText[dfsLowerSalaryIndex: dfsLowerSalaryIndex + 7]
-        dfsAmount = dfsSalaryText[1:]
-        dfsSalary = int(dfsAmount[:dfsAmount.find(',')] + dfsAmount[dfsAmount.find(',') + 1:])
-        if dfsSalary == 0:
-            continue
-        dfsSalaryList.append(dfsSalary)
-        dfsLowerStatsIndex = dfsLowerSalaryIndex + 14
-        dfsUpperStatsIndex = dfsLowerStatsIndex + 50
-        statsList = dfsText[dfsLowerStatsIndex: dfsUpperStatsIndex].split()
-        fppg = float(statsList[3])
-        dfsPointsList.append(fppg)
-        dfsPosition = dfsText[index - 3: index].strip()
-        if dfsPosition.startswith('SP'):
-            dfsPosition = 'P'
-        if site == 'k':
-            if len(dfsPosition) == 1:
-                dfsPosition = position_dict.get(dfsPosition)
-            else:
-                pos1 = position_dict.get(dfsPosition[0])
-                pos2 = position_dict.get(dfsPosition[1])
-                dfsPosition = pos1 + ' ' + pos2
-        dfsPositionsList.append(dfsPosition)
-        dfsLastNameIndex = dfsText.find(',', dfsLowerNameIndex)
-        dfsLastName = dfsText[dfsLowerNameIndex: dfsLastNameIndex]
-        dfsFirstName = (dfsText[dfsLastNameIndex + 1: dfsUpperNameIndex])[1:]
-        dfsPlayerName = str(dfsFirstName) + ' ' + str(dfsLastName)
-        dfsPlayerList.append(dfsPlayerName)
+        if dfsText.find(' 1<', index + 300, index + 450) != -1 or dfsText.find('<b>1</b>', index + 300, index + 450) != -1:
+            dfsLowerNameIndex = dfsText.find('>', index) + 1
+            dfsUpperNameIndex = dfsText.find('<', dfsLowerNameIndex)
+            dfsLowerSalaryIndex = dfsText.find('</font>', dfsUpperNameIndex) - 7
+            dfsPlayer = dfsText[dfsLowerNameIndex: dfsUpperNameIndex]
+            dfsSalaryText = dfsText[dfsLowerSalaryIndex: dfsLowerSalaryIndex + 7]
+            dfsAmount = dfsSalaryText[1:]
+            dfsSalary = int(dfsAmount[:dfsAmount.find(',')] + dfsAmount[dfsAmount.find(',') + 1:])
+            if dfsSalary == 0:
+                continue
+            dfsSalaryList.append(dfsSalary)
+            dfsLowerStatsIndex = dfsLowerSalaryIndex + 14
+            dfsUpperStatsIndex = dfsLowerStatsIndex + 50
+            statsList = dfsText[dfsLowerStatsIndex: dfsUpperStatsIndex].split()
+            fppg = float(statsList[3])
+            dfsPointsList.append(fppg)
+            dfsPosition = dfsText[index - 3: index].strip()
+            if dfsPosition.startswith('SP'):
+                dfsPosition = 'P'
+            if site == 'k':
+                if len(dfsPosition) == 1:
+                    dfsPosition = position_dict.get(dfsPosition)
+                else:
+                    pos1 = position_dict.get(dfsPosition[0])
+                    pos2 = position_dict.get(dfsPosition[1])
+                    dfsPosition = pos1 + ' ' + pos2
+            dfsPositionsList.append(dfsPosition)
+            dfsLastNameIndex = dfsText.find(',', dfsLowerNameIndex)
+            dfsLastName = dfsText[dfsLowerNameIndex: dfsLastNameIndex]
+            dfsFirstName = (dfsText[dfsLastNameIndex + 1: dfsUpperNameIndex])[1:]
+            dfsPlayerName = str(dfsFirstName) + ' ' + str(dfsLastName)
+            dfsPlayerList.append(dfsPlayerName)
 
     # put players and salaries into dictionary
     dfsPlayersAndSalaries = dict(zip(dfsPlayerList, dfsSalaryList))
