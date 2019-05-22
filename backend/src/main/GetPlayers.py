3e7fac807595d1c4493a373904f3206b6f3acab2
Adajian <dadajian@stats.com>
2019-05-22 13:38:59 -0500

    Updated player list to 2019

--- backend/src/main/GetPlayers.py
+++ backend/src/main/GetPlayers.py
@@ -4,34 +4,28 @@ from bs4 import BeautifulSoup
 
 # ranked player list that everyone drafts from
 session = requests.session()
-request = session.get('http://www.espn.com/fantasy/football/story/_/page/18RanksPreseason300nonPPR/'
-                      + '2018-fantasy-football-non-ppr-rankings-top-300')
+request = session.get('http://www.espn.com/fantasy/football/story/_/id/26692058/fantasy-football-updated-2019-non-ppr-rankings-mike-clay')
 doc = BeautifulSoup(request.content, 'html.parser')
 text = str(doc.get_text)
 
 string = ''
 for line in text.splitlines():
-    if 'section class="col-c chk-height nocontent"' in line:
+    if 'Non-PPR Top 300 for 2019' in line:
         string = line
 
 words = string.split('<')
 
 top300List = []
-numList = list(str(range(200)))
+numList = list(str(range(300)))
 for word in words:
     if 'http://www.espn.com/nfl/player/_/id/' in word:
         name = word.split('>')
-        if 'http://' not in name:
-            top300List.append(name[1])
+        top300List.append(name[1])
+    elif 'td>' in word and '. ' in word and word.split('. ')[1]:
+        top300List.append(word.split('. ')[1])
     elif 'D/ST' in word:
         dName = word.split('. ')
-        if 'td>' not in dName:
-            top300List.append(dName[1])
-    elif 'http://www.espn.com/nfl/player/_/id/' not in word and 'td>' in word and any(
-            num in word for num in numList) and '.' in word:
-        otherName = word.split('. ')
-        if 'td>' not in otherName and otherName[1] != '':
-            top300List.append(otherName[1])
+        top300List.append(dName[1])
 
 top300Positions = []
 pos_list = ['td>QB', 'td>RB', 'td>WR', 'td>TE', 'td>DST', 'td>K']
@@ -39,7 +33,7 @@ for word in words:
     if any(pos == word for pos in pos_list) and all(num not in word for num in numList):
         position = word.split('>')
         top300Positions.append(position[1])
-    
+
 top300dict = dict(zip(top300List, top300Positions))
 
 final_player_list = [str(player) + '    (' + str(pos) + ')' for player, pos in top300dict.items()]
