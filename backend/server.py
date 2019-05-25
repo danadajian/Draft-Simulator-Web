3bc799584a6db4a9c08b95434ba81533827d58cc
danadajian <danadajian@gmail.com>
2019-05-25 12:08:42 -0500

    Tryna fix the port issue

--- backend/server.py
+++ backend/server.py
@@ -206,5 +206,5 @@ def run_draft():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='127.0.0.1', port=port)
+    port = int(os.environ.get("PORT", 5432))
+    app.run(host='0.0.0.0', port=port)
