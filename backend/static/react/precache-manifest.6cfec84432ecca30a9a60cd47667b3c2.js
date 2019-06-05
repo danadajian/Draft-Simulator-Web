f8d558ed6cc187705f78b5fa6d0e0f783a2e004c
Adajian <dadajian@stats.com>
2019-06-05 17:11:26 -0500

    Prettied up the home page a little

---
+++ backend/static/react/precache-manifest.6cfec84432ecca30a9a60cd47667b3c2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b01e6120d05abd33918d1dbb78c0660f",
+    "url": "/static/react/media/map-saturation-overlay.b01e6120.png"
+  },
+  {
+    "revision": "076dc20edf52be6efbb83c7dd09b84fa",
+    "url": "/static/react/media/map-hue.076dc20e.png"
+  },
+  {
+    "revision": "c5005feba5d92371ab2c34bb97965345",
+    "url": "/static/react/media/football.c5005feb.ico"
+  },
+  {
+    "revision": "53eac5d40d2daff628d2",
+    "url": "/static/react/js/runtime~main.5d7886d3.js"
+  },
+  {
+    "revision": "e88d3840326fd5ca3206",
+    "url": "/static/react/js/main.74be1a1d.chunk.js"
+  },
+  {
+    "revision": "151f5cd1f19ecd030e47",
+    "url": "/static/react/js/2.1e5d95dd.chunk.js"
+  },
+  {
+    "revision": "e88d3840326fd5ca3206",
+    "url": "/static/react/css/main.f15363be.chunk.css"
+  },
+  {
+    "revision": "151f5cd1f19ecd030e47",
+    "url": "/static/react/css/2.fbeefa47.chunk.css"
+  },
+  {
+    "revision": "7263f511a284fcd2a9cb72192358dfb9",
+    "url": "/static/react/../../templates/index.html"
+  }
+];
