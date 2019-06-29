5f2627117fb30bb170c16c86fc55bcae11d0c2f4
Adajian <dadajian@stats.com>
2019-06-29 15:49:32 -0500

    Finished API setup to get today's FD and DK projections and salaries

--- backend/src/main/apiSetup.py
+++ backend/src/main/apiSetup.py
@@ -22,22 +22,16 @@ signature = hashlib.sha256(encoded_string).hexdigest()
 now = datetime.datetime.now()
 dateString = str(now.year) + '-' + str(now.month) + '-' + str(now.day)
 
-host = 'http://api.stats.com/v1/stats/'
-
 
 def call_api(endpoint):
     params = '?accept=json&api_key=' + api_key + '&sig=' + signature
-    response = requests.get(host + endpoint + params).json()
+    response = requests.get('http://api.stats.com/v1/stats/' + endpoint + params).json()
     return response
 
 
 eventsEndpoint = 'baseball/mlb/events/?date=' + dateString
-
 events = call_api(eventsEndpoint).get('apiResults')[0].get('league').get('season').get('eventType')[0].get('events')
-
 eventIds = [event.get('eventId') for event in events]
-
-print(eventIds)
 
 projectionsEndpoint = 'baseball/mlb/fantasyProjections/'
 projResponses = [call_api(projectionsEndpoint + str(eventId)).get('apiResults')[0].get('league').get('season').get('eventType')[0].get('fantasyProjections').get('teams') for eventId in eventIds]
@@ -48,14 +42,17 @@ batters = [{batter.get('playerId'): batter.get('fantasyProjections')} for team i
 pitchers = [{pitcher.get('playerId'): pitcher.get('fantasyProjections')} for team in pitcherData for pitcher in team]
 players = batters + pitchers
 
-projEndpoint = 'baseball/mlb/participants/teams/'
-teamLists = [call_api(projEndpoint + str(teamId)).get('apiResults')[0].get('league').get('players') for teamId in teams]
+teamsEndpoint = 'baseball/mlb/participants/teams/'
+participantsEndpoint = 'baseball/mlb/participants/'
+teamLists = [call_api(teamsEndpoint + str(teamId)).get('apiResults')[0].get('league').get('players') for teamId in teams]
 playerNames = {player.get('playerId'): player.get('firstName') + ' ' + player.get('lastName')
                for teamList in teamLists for player in teamList}
-print(playerNames)
 
 playerIdProjections = dict(zip([list(player.keys())[0] for player in players], [list(player.values())[0] for player in players]))
 
-playerProjections = dict(zip([playerNames.get(id) for id in playerIdProjections.keys()], playerIdProjections.values()))
+playerProjections = dict(zip([playerNames.get(id) if playerNames.get(id)
+                              else call_api(participantsEndpoint + str(id)).get('apiResults')[0].get('league').get('players')[0].get('firstName') + ' '
+                              + call_api(participantsEndpoint + str(id)).get('apiResults')[0].get('league').get('players')[0].get('lastName')
+                              for id in playerIdProjections.keys()], playerIdProjections.values()))
 
 print(playerProjections)
