addca83400532c4f86df93906cc7925cb48c0585
Adajian <dadajian@stats.com>
2019-08-05 22:18:31 -0500

    Added player search bar and user ranking adjustment features to simulator

--- frontend/src/PlayerListBox.tsx
+++ frontend/src/PlayerListBox.tsx
@@ -9,12 +9,15 @@ interface playerAttributes {
 
 interface playerProps {
     player: playerAttributes,
-    isAdd: boolean,
-    onChange: () => void
+    isUserPlayer: boolean,
+    onChange: () => void,
+    onMove: (direction: string) => void
 }
 
 const plus = require("./icons/plus.ico") as any;
 const minus = require("./icons/minus.ico") as any;
+const up = require("./icons/up.ico") as any;
+const down = require("./icons/down.ico") as any;
 
 const Player = (props: playerProps) =>
     <tr>
@@ -24,24 +27,49 @@ const Player = (props: playerProps) =>
             <tr>{props.player.Team} {props.player.Position}</tr>
         </td>
         <td>
-            <img src={(props.isAdd) ? plus : minus} alt={"add-or-remove"} onClick={props.onChange} style={{height: '4vmin'}}/>
+            <img src={(props.isUserPlayer) ? minus : plus} alt={"add-or-remove"} onClick={props.onChange} style={{height: '3vmin'}}/>
         </td>
+        {props.isUserPlayer && (
+            <td style={{display: 'flex', flexDirection: 'column'}}>
+                <img src={up} alt={"up"} onClick={() => props.onMove('up')} style={{height: '3vmin'}}/>
+                <img src={down} alt={"down"} onClick={() => props.onMove('down')} style={{height: '3vmin'}}/>
+            </td>
+            )
+        }
     </tr>;
 
-export const PlayerListBox = (props: { playerList: playerAttributes[], addPlayer: (index: number) => void }) =>
-    <table style={{ borderCollapse: 'collapse'}} className={'Draft-grid'}>
-        {props.playerList.map(
-            (player, index) => (
-                <Player player={player} isAdd={true} onChange={() => props.addPlayer(index)}/>
-            )
-        )}
-    </table>;
+export const PlayerListBox = (props: {
+    playerList: playerAttributes[],
+    filterList: playerAttributes[],
+    addPlayer: (index: number) => void }) =>
+        <table style={{ borderCollapse: 'collapse'}} className={'Draft-grid'}>
+            {props.playerList.map(
+                (player, index) => {
+                    if (!props.filterList || props.filterList.includes(player)) {
+                        return (
+                            <Player player={player}
+                                        isUserPlayer={false}
+                                        onChange={() => props.addPlayer(index)}
+                                        onMove={null}
+                            />
+                        )
+                    }
+                }
+            )}
+        </table>;
 
-export const UserListBox = (props: { playerList: playerAttributes[], removePlayer: (index: number) => void }) =>
-<table style={{borderCollapse: 'collapse'}} className={'Draft-grid'}>
-    {props.playerList.map(
-        (player, index) => (
-            <Player player={player} isAdd={false} onChange={() => props.removePlayer(index)}/>
-        )
-    )}
-</table>;
+export const UserListBox = (props: {
+    playerList: playerAttributes[],
+    removePlayer: (index: number) => void,
+    movePlayer: (index: number, direction: string) => void}) =>
+        <table style={{borderCollapse: 'collapse'}} className={'Draft-grid'}>
+            {props.playerList.map(
+                (player, index) => (
+                    <Player player={player}
+                            isUserPlayer={true}
+                            onChange={() => props.removePlayer(index)}
+                            onMove={(direction) => props.movePlayer(index, direction)}
+                    />
+                )
+            )}
+        </table>;
