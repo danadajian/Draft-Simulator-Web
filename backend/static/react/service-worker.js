7e728f1fe2941fcc56803570e556ba204e09804e
danadajian <danadajian@gmail.com>
2019-08-12 19:11:12 -0500

    Separated espn and yahoo ranking save and load, also made api key configs more dynamic

--- backend/static/react/service-worker.js
+++ backend/static/react/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/static/react/precache-manifest.b2caf5473c35a603bbd9b7363c67763f.js"
+  "/static/react/precache-manifest.69967659964ae8bab30cff9714671695.js"
 );
 
 workbox.clientsClaim();
