5dae381080367e9fae93a2c4a9b2e645d0f70c2d
unknown <danadajian@gmail.com>
2019-08-23 23:17:00 -0500

    Fixed mistake in server file and also improved simulator player caching

--- backend/src/test/GetPlayersTests.py
+++ backend/src/test/GetPlayersTests.py
@@ -9,11 +9,11 @@ class GetPlayersTests(unittest.TestCase):
 
     def test_player_names_valid(self):
         for player in top300List:
-            self.assertTrue(player.isalpha() or any(x in player for x in [' ', '.', "'", 'D/ST']))
+            self.assertTrue(player.isalpha() or any(x in player for x in [' ', '.', "'", 'DST']))
 
     def test_pos_names_valid(self):
         for pos in top300List:
-            if not pos != 'D/ST':
+            if not pos != 'DST':
                 self.assertTrue(pos.isalpha())
 
 
